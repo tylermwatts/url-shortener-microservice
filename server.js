82abd088858ba7d8cfd6d452a2ff736d144ef6d3
Glitch (rainbow-sphere) <none>
2018-12-20 01:03:09 +0000

    🐫🐑 Checkpoint ./server.js:2521121/320 ./package.json:2521121/26

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI);
+const Schema = mongoose.Schema();
+const dns = require('dns');
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
@@ -30,6 +33,9 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+app.route('/api/shorturl/new')
+  .get((req,res)=>{})
+
 
 
 app.listen(port, function () {
